28530e023a4d47bc51670466f3bb7d91aa2e77ac
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-08-16 07:18:41 +0100

    initial commit

--- src/components/SingleDonation.jsx
+++ src/components/SingleDonation.jsx
@@ -5,19 +5,12 @@ import { checkUser } from "../utils/functions";
 import { singleUser } from "../features/user/userSlice";
 
 function SingleDonation({ donor }) {
-  const [open, setOpen] = useState(false);
-
-  const closeModal = () => {
-    setOpen(false);
-  };
-
-  const dispatch = useDispatch();
   useEffect(() => {}, []);
   return (
     <div className=' mb-5   flex items-start justify-between gap-8 bg-grey py-3 px-5 rounded text-black'>
       <div className='cursor-pointer'>
         <h6 className='text-md font-extrabold'>{donor?.charity?.title}</h6>
-        <p className='text-sm my-2 text-wrap'>{donor?.charity?.description}</p>
+        <p className='text-sm my-2  truncate'>{donor?.charity?.description}</p>
         <span className='italic text-zinc-700 text-xs  mt-2'>
           by {checkUser(donor?.user)}
         </span>
