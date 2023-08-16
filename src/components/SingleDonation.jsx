a4f9857d408496a0a470c01158e477783faa3b36
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-08-16 07:48:50 +0100

    initial commit

--- src/components/SingleDonation.jsx
+++ src/components/SingleDonation.jsx
@@ -10,7 +10,9 @@ function SingleDonation({ donor }) {
     <div className=' mb-5   flex items-start justify-between gap-8 bg-grey py-3 px-5 rounded text-black'>
       <div className='cursor-pointer'>
         <h6 className='text-md font-extrabold'>{donor?.charity?.title}</h6>
-        <p className='text-sm my-2  truncate'>{donor?.charity?.description}</p>
+        <p className='text-sm my-2  truncate text-wrap'>
+          {donor?.charity?.description}
+        </p>
         <span className='italic text-zinc-700 text-xs  mt-2'>
           by {checkUser(donor?.user)}
         </span>
