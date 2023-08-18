ffc44e631525ea2cf8b1330fcdb68f0133dfdd0e
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-08-18 07:12:55 +0100

    initial commit

--- src/features/donations/singleDonationThunk.js
+++ src/features/donations/singleDonationThunk.js
@@ -1,20 +1,18 @@
 import { toast } from "react-toastify";
 import { customUrl } from "../../utils/axios";
 import { checkUserAuthorization } from "../../utils/functions";
-// import { getRecipeReviews } from "../reviews/reviewsSlice";
-// import { getSingleRecipe } from "../singleRecipe/singleRecipeSlice";
 import { clearState, getCharityDonations } from "./singleDonationSlice";
 import { getSingleCharity } from "../singleCharity/singleCharitySlice";
 
 const createDonationThunk = async (body, thunkAPI) => {
   try {
-    console.log(body);
     const resp = await customUrl.post("/donations", body, {
       withCredentials: true,
     });
-    thunkAPI.dispatch(getCharityDonations(resp.data.review.charity));
-    thunkAPI.dispatch(getSingleCharity(resp.data.donation.charity));
+    // thunkAPI.dispatch(getCharityDonations(resp.data.charity));
+    // thunkAPI.dispatch(getSingleCharity(resp.data.donation.charity));
     thunkAPI.dispatch(clearState());
+    console.log(resp.data);
     return resp.data;
   } catch (error) {
     return checkUserAuthorization(error, thunkAPI);
@@ -41,7 +39,7 @@ const getCharityDonationThunk = async (id, thunkAPI) => {
     console.log(resp.data);
     return resp.data;
   } catch (error) {
-    return checkUserAuthorization(error, thunkAPI);
+    return thunkAPI.rejectWithValue(error.response.data.msg);
   }
 };
 export { createDonationThunk, getUserDonationThunk, getCharityDonationThunk };
