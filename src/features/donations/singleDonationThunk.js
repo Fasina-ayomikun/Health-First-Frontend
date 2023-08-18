537ab40b858ea247eb223cf337a7e0fa7f487036
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-08-18 08:56:48 +0100

    initial commit

--- src/features/donations/singleDonationThunk.js
+++ src/features/donations/singleDonationThunk.js
@@ -39,7 +39,8 @@ const getCharityDonationThunk = async (id, thunkAPI) => {
 
     return resp.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data.msg);
+     return checkUserAuthorization(error, thunkAPI);
+ 
   }
 };
 export { createDonationThunk, getUserDonationThunk, getCharityDonationThunk };
