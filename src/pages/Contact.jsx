299b88dcb3c66e9267405c485a59d2b148dded6f
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-08-22 23:04:17 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,35 +1,9 @@
 import React, { useRef, useState } from "react";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import emailjs from "@emailjs/browser";
 import { toast } from "react-toastify";
 function ContactPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const form = useRef();
-  const inputRef = useRef();
-  const textRef = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID, //service id
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID, //template id
-        form.current,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY //public key
-      )
-      .then(
-        (result) => {
-          toast.success("Message sent successfully");
-          setIsLoading(false);
-          inputRef.current.value = "";
-          textRef.current.value = "";
-        },
-        (error) => {
-          toast.error(error);
-        }
-      );
-  };
+  
   return (
     <>
       <Hero text='Contact' />
@@ -37,7 +11,7 @@ function ContactPage() {
         <h3 className='underline text-3xl underline font-semibold capitalize text-center my-4'>
           Contact Us
         </h3>
-        <form ref={form} onSubmit={sendEmail}>
+        <form >
           <div className=' mt-16 '>
             <label htmlFor='email' className='block text-black'>
               Email:
@@ -45,7 +19,7 @@ function ContactPage() {
             <input
               type='email'
               id='email'
-              ref={inputRef}
+              
               required
               name='user_email'
               className='mb-5 text-grey block bg-transparent border-black border-b-2  w-full rounded h-10 px-3 '
@@ -56,7 +30,7 @@ function ContactPage() {
             <textarea
               type='text'
               name='message'
-              ref={textRef}
+             
               id='message'
               required
               className='resize-none  text-black bg-transparent  border-b-2  w-full  rounded h-20  px-3 border-black '
@@ -64,15 +38,12 @@ function ContactPage() {
           </div>
           <button
             type='submit'
-            onClick={() =>
-              setTimeout(() => {
-                setIsLoading(true);
-              }, 1000)
-            }
-            disabled={isLoading ? true : false}
+            
+            
             className='capitalize border-2 py-3 px-16  flex  rounded  mx-auto flex my-12 text-black border-black'
           >
-            {isLoading ? "Sending..." : "Send"}
+            Send
+
           </button>
         </form>
       </section>
