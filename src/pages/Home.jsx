44381886c07cd1d3b7e71f1a704fe895fb99f1ae
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-08-25 14:42:22 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 function Home() {
   return (
     <>
-      <Hero text='Healthy First' />;
+      <Hero text='Health First' />;
       <Why />
       <Testimonial />
       <Footer />
